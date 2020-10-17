7acfab985f8c54c77825cc1c0c0ce703a7d273de
TiagoGrosseli <73004783+TiagoGrosseli@users.noreply.github.com>
2020-10-16 22:38:02 -0300

    first commit

---
+++ agenda/src/components/Menu.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="navegacao">
+        Menu da Aplicação
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="stylus" scoped>
+#navegacao{
+    width:100%;
+    height:10%;
+    background-color: black;
+}
+
+</style>
